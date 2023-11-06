7263b63e1654b8b67efef078f83a1b1a8d39ca7f
stewie-gil <gilliganstewie7@gmail.com>
2023-11-06 12:05:17 +0300

    build

--- backend/app.js
+++ backend/app.js
@@ -100,6 +100,13 @@ const connectDB = async () => {
 };
 
 
+app.use(express.static(path.join(__dirname, './frontend/build')));
+app.get("*", (req, res)=>{
+
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+})
+
+
 connectDB().then(() => {
   server.listen(port, () => {
     console.log(`API is running on port ${port}`);
